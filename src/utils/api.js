dc9a0134d1ded958164bceb96181c011d2be0199
Janet Castillo <janetcastillo56@gmail.com>
2023-11-16 18:51:35 -0700

    edits

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://backend-21cc.onrender.com/";
+  process.env.REACT_APP_API_BASE_URL || "https://backend-21cc.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
